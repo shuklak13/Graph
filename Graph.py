97ec7aeb9d00ff9e02bd47599e14671a1bfe690b
Karan Shukla <shuklak13@gmail.com>
2018-09-15 17:47:09 -0500

    add more functions to Graph.py

--- Graph.py
+++ Graph.py
@@ -9,8 +9,20 @@ class Graph:
     def get_edge(self, s, t):
         return self.edges[s].get(t, None)
 
-    def get_nbrs(self, s):
+    def nodes(self):
+        return self.edges.keys()
+
+    def nbrs(self, s):
         return self.edges[s].keys()
+
+    def __getitem__(self, s):
+        return self.edges[s]
+
+    def __len__(self):
+        return len(self.edges)
+
+    def __iter__(self):
+        return self.edges.__iter__()
 
     def __str__(self):
         return str(self.edges)
